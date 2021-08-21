14c45aed55bc8ceb8217f31414af7d1acc1c40b4
WooJongSeong <whdtud@gmail.com>
2021-08-21 20:31:22 +0900

    toss 문제 1 등록

--- project/src/com/joongang/Main.java
+++ project/src/com/joongang/Main.java
@@ -3,6 +3,12 @@ package com.joongang;
 public class Main {
 
 	public static void main(String[] args) {
+
+		String s = "";
 		
+		com.toss.Problem01 p1 = new com.toss.Problem01();
+		String result = p1.solution(s);
+		
+		System.out.println(result);
 	}
 }
