1ad2826e36308170e9975c1e2dedc193c3f433fb
WooJongSeong <whdtud@gmail.com>
2021-09-02 01:55:50 +0900

    프로그래머스 해쉬 level3 문제 풀이

--- project/src/main/Main.java
+++ project/src/main/Main.java
@@ -1,12 +1,12 @@
 package main;
 
-import programmers.kit.hash.Level2;
+import programmers.kit.hash.Level3;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		Level2 l = new Level2();
+		Level3 l = new Level3();
 		l.main();
 	}
 }
